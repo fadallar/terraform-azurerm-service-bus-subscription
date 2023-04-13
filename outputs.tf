7eda304a5b6a9ef0b3f7db832ac91214b2cb1515
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-04-13 14:48:10 +0200

    Adding extra outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "service_bus_topic_id" {
+output "service_bus_subscription_id" {
   value       = azurerm_servicebus_subscription.subscription.id
   description = "Service Bus Subscription Id"
 }
@@ -8,7 +8,23 @@ output "id_sb_data_receiver" {
   description = "Id to the user-managed identity with Service Bus Data Receiver"
 }
 
+
 output "id_sb_data_sender" {
   value       = azurerm_user_assigned_identity.sb_data_sender.id
   description = "Id to the user-managed identity with Service Bus Data Sender"
+}
+
+output "clientid_sb_data_receiver" {
+  value       = azurerm_user_assigned_identity.sb_data_receiver.client_id
+  description = "Client Id to the user-managed identity with Service Bus Data receiver Role"
+}
+
+output "principalid_sb_data_receiver" {
+  value       = azurerm_user_assigned_identity.sb_data_receiver.principal_id
+  description = "Principal Id to the user-managed identity with Service Bus Data receiver Role"
+}
+
+output "tenantid_sb_data_receiver" {
+  value       = azurerm_user_assigned_identity.sb_data_receiver.tenant_id
+  description = "Tenant Id to the user-managed identity with Service Bus Data receiver Role"
 }
